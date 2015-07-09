52ebc5c90b5b2738e168797f040c85f52246be0c
Francisco Javier <fjgomez2@gmail.com>
2015-07-09 10:57:05 +0200

    2. Identificación de versión

--- app.js
+++ app.js
@@ -4,26 +4,36 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+// Importa paquetes con middlewares
+//Middleware: Función JavaScript que se ejecuta al llegar una transacción HTTP.
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+//var users = require('./routes/users');
+//dice que hay que borrar la línea de arriba.
+// Importa paquetes con middlewares de enrutadores
 
 var app = express();
+//Crear aplicación
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+//Instalar generador de vistas EJS
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+//Instalar los middlewares importados.
 
 app.use('/', routes);
-app.use('/users', users);
+//app.use('/users', users);
+//Dice que hay que borrar la línea de arriba.
+//Instalar los middlewares enrutadores importados
+//Asociar rutas a sus gestores.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -31,6 +41,7 @@ app.use(function(req, res, next) {
     err.status = 404;
     next(err);
 });
+//Resto de rutas: genera error 404 de HTTP.
 
 // error handlers
 
@@ -45,6 +56,7 @@ if (app.get('env') === 'development') {
         });
     });
 }
+//Gestión de errores durante el desarrollo
 
 // production error handler
 // no stacktraces leaked to user
@@ -58,3 +70,4 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
+//Exportar app para comando de arranque
