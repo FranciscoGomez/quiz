aa9702ac284f9e7391e6e9e2f6973810c04b170e
Francisco Javier <fjgomez2@gmail.com>
2015-07-26 19:37:34 +0200

    Práctica 7.2

--- models/models.js
+++ models/models.js
@@ -39,7 +39,7 @@ exports.Quiz = Quiz;	//exportar definción de tabla Quiz
 sequelize.sync().then(function(){
 	// success(..) ejecuta el manejador una vez creada la tabla
   Quiz.count().then(function (count) {
-	if(count === 0) {	//la tabla se inicializa solo si está vacía
+	if(count === 1) {	//la tabla se inicializa solo si está vacía
 	   Quiz.create({ pregunta: 'Capital de Italia',
 			 respuesta: 'Roma'
 			})
