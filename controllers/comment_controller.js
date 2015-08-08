680c2d33bc0d6173584c809b3556f5807c03787e
Francisco Javier <fjgomez2@gmail.com>
2015-08-08 23:24:16 +0200

    9.4.Moderación de comentarios

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,5 +1,20 @@
 var models = require('../models/models.js');
 
+//Autoload :id de comentarios
+exports.load = function(req, res, next, commentId) {
+	models.comment.find({
+		where: {
+			id: Number(commentId)
+		}
+		}).then(function(comment) {
+		if (comment) {
+			req.comment = comment;
+			next();
+		} else{next(new Error('No existe commentId=' + commentId))}
+		}
+	).catch(function(error){next(error)});
+};
+
 // GET /quizes/:quizId/comments/new
 exports.new = function(req, res) {
 	res.render('comments/new.ejs', {quizid: req.params.quizId, errors: []});
@@ -21,4 +36,12 @@ comment.validate().then(function(err) {
 	}
 	}
 	).catch(function(error){next(error)});
+};
+
+//GET /quiz/:quizId/comments/:commentId/publish
+exports.publish = function (req, res) {
+	req.comment.publicado = true;
+
+	req.comment.save( {fields: ["publicado"]})
+		.then(function(){ res.redirect('/quizes/'+req.params.quizId);})
 };
--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,8 @@ router.get('/', function(req, res) {
 //router.get('/quizes/question', quizController.question);
 //router.get('/quizes/answer', quizController.answer);
 
-router.param('quizId', 					quizController.load); // autoload : quizId
+router.param('quizId', 					quizController.load); 		// autoload : quizId
+router.param('commentId', 				commentController.load); 	//autoload: commentId
 
 //Definición de rutas de sesión
 router.get('/login', 					sessionController.new); 	//formulario login
@@ -45,8 +46,10 @@ router.get('/quizes/:quizId(\\d+)/edit',		sessionController.loginRequired, quizC
 router.put('/quizes/:quizId(\\d+)',			sessionController.loginRequired, quizController.update);
 router.delete('/quizes/:quizId(\\d+)', 			sessionController.loginRequired, quizController.destroy);
 
+//Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', 	commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',		commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 //author se toca en los siguientes archivos:
 // /routes/index.js
