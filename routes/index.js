f3369f8d7a6e4e1d1ba023dfc99c7058565b9159
Francisco Javier <fjgomez2@gmail.com>
2015-08-08 22:57:22 +0200

    9.3. Autorización

--- routes/index.js
+++ routes/index.js
@@ -38,12 +38,12 @@ router.get('/quizes/:quizId(\\d+)/answer',		quizController.answer);
 router.get('/quizes/author', 				quizController.author);
 //llama al archivo: /vies/quizes/author.ejs
 
-router.get('/quizes/new',				quizController.new);
-router.post('/quizes/create',				quizController.create);
+router.get('/quizes/new',				sessionController.loginRequired, quizController.new);
+router.post('/quizes/create',				sessionController.loginRequired, quizController.create);
 
-router.get('/quizes/:quizId(\\d+)/edit',		quizController.edit);
-router.put('/quizes/:quizId(\\d+)',			quizController.update);
-router.delete('/quizes/:quizId(\\d+)', 			quizController.destroy);
+router.get('/quizes/:quizId(\\d+)/edit',		sessionController.loginRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)',			sessionController.loginRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)', 			sessionController.loginRequired, quizController.destroy);
 
 router.get('/quizes/:quizId(\\d+)/comments/new', 	commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',		commentController.create);
